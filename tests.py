bcbd9e2333f3e586bb8eba487a59182398c3a1ad
ephe <ephe1@icloud.com>
2022-03-31 17:24:01 +0100

    tests : added assert tests

--- tests.py
+++ tests.py
@@ -1,16 +1,24 @@
-from unittest import TestCase, main
-import exceptions
-
-# ?mocking/what is this/definition...
-
-# check response is an array of questions w/ answers(t/f)
-        # ...as opposed to the url
-
-# check it asks a question
-
-# check it excepts t/f as a response in any case
-
-# check it tots up correct score
+import trivia
 
 
+def test_url():
+    assert trivia.response == "https://opentdb.com/api.php?amount=10&category=20&type=boolean"
 
+
+def player_answer():
+    assert trivia.player_answer(bool)
+
+
+# FUNCTION QUIZ TESTS
+# check for url
+
+# # check url ok
+# def get_url():
+#     # arrange/connect to api
+#     url = "https://opentdb.com/api.php?amount=10&category=17&type=boolean"
+#     # act/get json questions
+#     response = requests.get(url)
+#     body = response.json()
+#     #  assert
+#     assert response.status_code == 200
+#     assert 'Python' in body['AbstractText']
--- trivia.py
+++ trivia.py
@@ -1,29 +1,64 @@
-# connect to api
 import requests
 import json
 
-# get statments
-# def det_statements():
-response = requests.get("https://opentdb.com/api.php?amount=10&category=17&type=boolean")
+ # get statments
+response = requests.get("https://opentdb.com/api.php?amount=10&category=20&type=boolean")
 questions = response.json()
 my_trivia = questions['results']
 count = 0
 
-# def play_quiz():
 for question in my_trivia:
-
     print("a trivia question:", question['question'])
 
-    # def player_answer()
     player_answer = input('True or False: ')
     answer = question['correct_answer']
 
     # set score
     if player_answer.lower() == answer.lower():
         count += 1
-
-        #     update score
-        #     update score
     print(count)
 
 print('congratulations, your score was ' + str(count))
+
+
+
+
+
+
+
+
+# ---------------------------------------
+
+# START OF QUIZ WITH FUNCTIONS
+# plan: pass each input to next function
+# make ui
+# add multiple choice option
+
+# ----
+
+# # get statments
+# def get_questions():
+#     response = requests.get("https://opentdb.com/api.php?amount=10&category=20&type=boolean")
+#     questions = response.json()
+#     return questions['results']
+#
+#
+# def get_question():
+#     for question in get_questions():
+#         print("a trivia question:", question['question'])
+#
+# def player_answer():
+#     player_answer = input('True or False: ')
+#     if player_answer.lower() == correct_answer.lower():
+#         count += 1
+#         #     update score
+#     print(count)
+#
+# print('congratulations, your score was ' + str(count)) mythology quiz! <3
+#
+# def main():
+#     questions = get_questions()
+#     for question in questions:
+#         answer = get_player_answer(question) # prints to screen, gets input, checks input, return bool
+#         if answer is correct:
+#             count + 1
